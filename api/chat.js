f5b5bdfdd27488636b09038cb83cb3fa4c877dad
fmcclinic <bsdothanhlong@gmail.com>
2025-12-15 19:29:52 +0700

    Remove commented note about generateSuggestedQuestions

--- api/chat.js
+++ api/chat.js
@@ -67,4 +67,4 @@ export default async function handler(req, res) {
         });
     }
 }
-// Lưu ý: Hàm generateSuggestedQuestions đã được loại bỏ để tránh lỗi.
+// Lưu ý: Hàm generateSuggestedQuestions đã được loại bỏ để tránh lỗi...
